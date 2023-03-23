72510b4b23938c08558eac1537b03f185be76d52
Grey Goo <greygoo@hno3.eu>
2023-03-23 11:27:15 +0000

    fix some bugs and made it more robust

--- restore_home/restore_home.sh
+++ restore_home/restore_home.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+WD=$(pwd)
+trap "cd ${WD}}" INT TERM
+cd /tmp
+
 if [ ! -d /.snapshots/restore_home ]; then
     echo "No restore snapshot of home found. Bailing out."
     exit 1
-elif
+else
     echo "These actions will be preformed:"
     echo "- Creating a snapshot of current /home"
     echo "- Restoring /home to originals state."
     echo "Please type "y" to continue"
-    read
-    if [ REPLY = "y" ]; then
+    read REPLY
+    if [ "${REPLY}" = "y" ]; then
         echo "Creating snapshot of current state"
-        btrfs subvolume snapshot /tmp/armbian/home /tmp/armbian/.snapshots/home_$(date +%m_%d_%Y-%H:%M)
+        btrfs subvolume snapshot /home /.snapshots/home_$(date +%m_%d_%Y-%H:%M)
 
         echo "Cleaning /home"
         rm -rf /home/*

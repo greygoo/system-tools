ad1ea946c8de2ab9bf611d69b9c09d94c61db606
greygoo <greygoo@hno3.eu>
2023-10-07 01:40:38 +0200

    Add script to install on emmc and create recovery snapshots

---
+++ emmc_install/emmc_install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $(id) -ne 0 ]; then
+  echo "You need to be root to run this script"
+  exit 1
+fi
+
+/usr/sbin/nand-sata-install
+
+rmdir /tmp/armbian
+mkdir /tmp/armbian
+
+mount /dev/mmcblk2p2 /tmp/armbian
+
+rmdir /tmp/armbian/.snapshots
+btrfs sub create /tmp/armbian/.snapshots
+btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_factory
+btrfs subvolume snapshot /tmp/armbian /tmp/armbian/.snapshots/root_current
+
+current_root_id=$(btrfs sub list /tmp/armbian/ | grep root_current | awk '{ print $2 }')
+btrfs sub set-default $current_root_id /tmp/armbian
+
+cat /tmp/armbian/etc/fstab
+btrfs subvolume list /tmp/armbian
+
+umount /tmp/armbian
+
+echo "Snapshots created"
